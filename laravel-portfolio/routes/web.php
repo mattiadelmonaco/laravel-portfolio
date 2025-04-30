a762f7f3d447fc58a4d3c0eebc87900ace8cccdd
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-30 18:05:35 +0200

    feat: add autentification middleware to route projects

--- laravel-portfolio/routes/web.php
+++ laravel-portfolio/routes/web.php
@@ -30,6 +30,7 @@ Route::middleware(['auth', 'verified'])
     ->name('profile');
 });
 
-Route::resource("projects", ProjectController::class);
+Route::resource("projects", ProjectController::class)
+->middleware(["auth", "verified"]);
 
 require __DIR__.'/auth.php';
