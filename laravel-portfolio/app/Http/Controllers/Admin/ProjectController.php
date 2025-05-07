94a03d536149e1020f4b9d4be02a8e1d7d9fc6b9
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-07 16:47:18 +0200

    feat: add controller to save technologies, add form's section to select technologies in create page

--- laravel-portfolio/app/Http/Controllers/Admin/ProjectController.php
+++ laravel-portfolio/app/Http/Controllers/Admin/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Technology;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
@@ -25,7 +26,8 @@ class ProjectController extends Controller
     public function create()
     {
         $types = Type::all();
-        return view("projects.create", compact("types"));
+        $technologies = Technology::all();
+        return view("projects.create", compact("types", "technologies"));
     }
 
     /**
@@ -45,6 +47,8 @@ class ProjectController extends Controller
         $newProject->type_id = $data["type_id"];
 
         $newProject->save();
+
+        $newProject->technologies()->attach($data["technologies"]);
 
         return redirect()->route("projects.show", $newProject->id);
 
