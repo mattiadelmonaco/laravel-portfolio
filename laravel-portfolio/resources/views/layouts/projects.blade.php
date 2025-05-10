4be93642c07aa9c532f799c498e38abdfa201aea
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-10 18:15:41 +0200

    feat: add favicon

--- laravel-portfolio/resources/views/layouts/projects.blade.php
+++ laravel-portfolio/resources/views/layouts/projects.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('mdm-backoffice.png') }}" type="image/x-icon">
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     {{-- FONT AWESOME --}}
