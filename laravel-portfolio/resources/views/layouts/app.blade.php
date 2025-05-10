014d7d986eba84cfb0ff78c75ba2814ca5f475b3
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-10 18:13:26 +0200

    feat: edit title and favicon

--- laravel-portfolio/resources/views/layouts/app.blade.php
+++ laravel-portfolio/resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>MDM - BackOffice</title>
+    <link rel="shortcut icon" href="{{ asset('mdm-backoffice.png') }}" type="image/x-icon">
 
 
     <!-- Fonts -->
